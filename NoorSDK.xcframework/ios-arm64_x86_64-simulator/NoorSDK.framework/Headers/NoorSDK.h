f947eab33067a78fc4724b71ffd87440002afcd1
Abdul Mottaleb <mottalebgakk@gmail.com>
2023-02-23 14:51:03 +0600

    Initial Commit

---
+++ NoorSDK.xcframework/ios-arm64_x86_64-simulator/NoorSDK.framework/Headers/NoorSDK.h
@@ -0,0 +1,18 @@
+//
+//  NoorSDK.h
+//  NoorSDK
+//
+//  Created by Azad on 1/2/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NoorSDK.
+FOUNDATION_EXPORT double NoorSDKVersionNumber;
+
+//! Project version string for NoorSDK.
+FOUNDATION_EXPORT const unsigned char NoorSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NoorSDK/PublicHeader.h>
+
+
---
+++ NoorSDKiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoorSDKiOS
+//
+//  Created by Gakk on 23/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
